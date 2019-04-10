93d090052e6a4286ed746aa000bd36e128961f25
Rupak Poddar <fin.rupak1983@gmail.com>
2019-04-10 21:32:50 +0300

    AB

--- src/com/anesh/poddar/MyClass.java
+++ src/com/anesh/poddar/MyClass.java
@@ -3,21 +3,8 @@ package com.anesh.poddar;
 public class MyClass {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hi, my name is Anesh");
-		System.out.println("Hi, my father is Rupak Poddar");
-		System.out.println("Hi, my mother is Limpa Ghosh");
-		
-		System.out.println("Hi, my name is Anesh");
-		System.out.println("Hi, my father is Rupak Poddar");
-		System.out.println("Hi, my mother is Limpa Ghosh");
-		
-		System.out.println("Hi, my name is Anesh");
-		System.out.println("Hi, my father is Rupak Poddar");
-		System.out.println("Hi, my mother is Limpa Ghosh");
-		
-		
-		
 
 	}
 
