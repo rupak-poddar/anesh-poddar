48e82b619854a7f5ca12c89b2ea375096635a320
Rupak Poddar <fin.rupak1983@gmail.com>
2019-04-10 21:45:42 +0300

    A

---
+++ src/com/anesh/poddar/MyClass2.java
@@ -0,0 +1,12 @@
+package com.anesh.poddar;
+
+public class MyClass2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi, my name is A");
+		System.out.println("Hi, my name is B");
+
+	}
+
+}
